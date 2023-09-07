adf081925ca28edc252d711751fa137cc232fd09
tomasmj503 <86091325+tomasmj503@users.noreply.github.com>
2023-09-07 02:20:00 +0000

    simple counter

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,36 @@
 import React from "react";
 
+
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import propTypes from "prop-types";
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="row box">
+			<div className="col counter"> 
+			<i className="far fa-clock"></i>
+			</div>
+
+			<div className="col counter">
+				{props.digit6}
+			</div>
+			<div className="col counter">
+				{props.digit5}
+			</div>
+			<div className="col counter">
+				{props.digit4}
+			</div>
+			<div className="col counter">
+				{props.digit3}
+			</div>
+			<div className="col counter">
+				{props.digit2}
+			</div>
+			<div className="col counter">
+				{props.digit1}
+			</div>
 		</div>
 	);
 };
--- src/js/index.js
+++ src/js/index.js
@@ -9,4 +9,20 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let seg = 0
+setInterval(() => {
+    let one = Math.floor((seg/1) % 10);
+    let two = Math.floor((seg/10) % 10);
+    let three = Math.floor((seg/100) % 10);
+    let four = Math.floor((seg/1000) % 10);
+    let five = Math.floor((seg/10000) % 10);
+    let six = Math.floor((seg/100000) % 10);
+    seg += 1;
+    ReactDOM.render(<Home digit1={one} digit2={two} digit3={three} digit4={four} digit5={five} digit6={six}  />, document.querySelector("#app"));
+}, 1000);
+
+
+
+
+
+
